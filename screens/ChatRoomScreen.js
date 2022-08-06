5548e804c4a65f66aced91bfe5e6317a0b54307b
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-08-06 17:19:08 +0800

    store user and messages they sent to firestore

--- screens/ChatRoomScreen.js
+++ screens/ChatRoomScreen.js
@@ -1,80 +1,61 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons'
-import * as Linking from 'expo-linking'
-import AppLoading from 'expo-app-loading'
-import React, { useState, useEffect, Component, useCallback } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Platform,
-  Alert,
-} from "react-native";
-import {
-  Bubble,
-  GiftedChat,
-  SystemMessage,
-  IMessage,
-  Send,
-  SendProps,
-} from 'react-native-gifted-chat'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 
+import { useNavigation } from "@react-navigation/native";
+import { GiftedChat } from "react-native-gifted-chat";
+import firebase from "../database/firebaseDB";
+const auth = firebase.auth();
+const db = firebase.firestore().collection("messages");
 
-
-import { commonStyles, darkStyles, lightStyles } from "../styles/commonStyles";
-import { useSelector } from "react-redux";
-import ChatScreen from "./ChatRoomScreen";
-
-
-export default function ChatRoomScreen({ navigation, route }) {
-  const token = useSelector((state)=>state.auth.token);
-  const isDark = useSelector((state) => state.accountPrefs.isDark);
-  const styles = { ...commonStyles, ...(isDark ? darkStyles : lightStyles) };
- 
+export default function ChatScreen() {
   const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState([]);
+  const navigation = useNavigation();
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ])
-  }, [])
+    const unsubscribe = db
+      .orderBy("createdAt", "desc")
+      .onSnapshot((collectionSnapshot) => {
+        const messages = collectionSnapshot.docs.map((doc) => {
+          const date = doc.data().createdAt.toDate();
+          const newDoc = { ...doc.data(), createdAt: date };
+          
+          return newDoc;
+        });
+        setMessages(messages);
+      });
 
-  const onSend = useCallback((messages = []) => {
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
+   
 
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons name="logout" size={30} color="grey" />
+        </TouchableOpacity>
+      ),
+    });
+    return unsubscribe;
+  }, []);
 
-  
+  const logout = () => auth.signOut();
+
+  function sendMessages(newMessages) {
+    console.log(newMessages);
+    db.add(newMessages[0]);
+  }
 
   return (
+    
+      
+
     <GiftedChat
       messages={messages}
-      onSend={messages => onSend(messages)}
-      user={{
-        _id: 1,
-      }}
+      onSend={sendMessages}
+      listViewProps={{ style: { backgroundColor: "white" } }}
+      user={{ _id: auth.currentUser?.uid, name: auth.currentUser?.email }}
     />
   );
 }
 
-const additionalStyles = StyleSheet.create({
-  chatButton: {
-    marginTop:100
-    
-  },
-  chatTitle: {
-    color:"red",
-  }
-
-  
-  
-});
+const styles = StyleSheet.create({});
